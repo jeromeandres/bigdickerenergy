88e96b498aafc311da3e4e80763af24ecfd2ab49
jeromeandres <152447312+jeromeandres@users.noreply.github.com>
2023-11-29 14:15:57 -0800

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "986721858638684160"; // your league ID
-export const leagueName = "Big Dicker Energy"; // your league name
+export const leagueName = "BIG DICKER ENERGY"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
